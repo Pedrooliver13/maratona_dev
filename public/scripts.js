ee113f0a53f6ee98706641b34884a3b93be6180e
Pedro Henrique de Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-18 12:51:05 -0300

    Add files via upload

---
+++ public/scripts.js
@@ -0,0 +1,7 @@
+const registerDoner = document.querySelector(".register")
+const appearForm = document.querySelector("#appearForm")
+
+appearForm.addEventListener("click" , ()=>{
+    registerDoner.classList.toggle("hide")
+
+})
